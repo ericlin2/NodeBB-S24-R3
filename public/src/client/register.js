25d01029bc80beb8fdaf9d70e5f472d96e79ca4a
ericlin2 <ericlin45209@gmail.com>
2024-09-16 12:08:30 -0400

    Modified error message to suggest a new username

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,8 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    let suggested = username + suffix;
+                    showError(username_notify, `[[error:username-taken, ${suggested}]]`);
                 }
 
                 callback();
